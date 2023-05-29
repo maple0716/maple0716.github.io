304f0981a2ea338714436adf486e220273eb098c
maple0716 <121430876+maple0716@users.noreply.github.com>
2023-05-29 13:17:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ function createARObject(latitude, longitude, text, latitude1, longitude1, text1)
 
     const textEntity = document.createElement('a-entity');
     textEntity.setAttribute('text', `value: ${text}; color: black; align: center; width: 10;`);
-    textEntity.setAttribute('position', '0 5 0');
+    textEntity.setAttribute('position', '0 10 0');
     entity.appendChild(textEntity);
 
     const entity1 = document.createElement('a-box');
     entity1.setAttribute('material', 'color: red;');
-    entity1setAttribute('scale', '20 20 20');
+    entity1.setAttribute('scale', '20 20 20');
     entity1.setAttribute('gps-entity-place', `latitude: ${latitude1}; longitude: ${longitude1}`);
     scene.appendChild(entity1);
 
     const textEntity1 = document.createElement('a-entity');
     textEntity1.setAttribute('text', `value: ${text1}; color: black; align: center; width: 10;`);
-    textEntity1.setAttribute('position', '0 5 0');
-    entity.appendChild(textEntity1);
+    textEntity1.setAttribute('position', '0 10 0');
+    entity1.appendChild(textEntity1);
 }
 createARObject(latitude, longitude, text, latitude1, longitude1, text1);
