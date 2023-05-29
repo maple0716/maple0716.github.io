547035ddf784d71901ea61cdf2fab1a9264d3ff9
maple0716 <121430876+maple0716@users.noreply.github.com>
2023-05-29 12:51:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const text= 'hello,weizhi1!';
 const latitude1 = 44.504417;
 const longitude1 = 121.4457913;
 const text1 = 'hello,weizhi2!';
-function createARObject(latitude, longitude, text) {
+function createARObject(latitude, longitude, text, latitude1, longitude1, text1) {
     const scene = document.querySelector('a-scene');
     const entity = document.createElement('a-box');
     entity.setAttribute('material', 'color: red;');
-    entity.setAttribute('scale', '5 5 5');
+    entity.setAttribute('scale', '20 20 20');
     entity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
     scene.appendChild(entity);
 
@@ -19,14 +19,13 @@ function createARObject(latitude, longitude, text) {
 
     const entity1 = document.createElement('a-box');
     entity1.setAttribute('material', 'color: red;');
-    entity1setAttribute('scale', '5 5 5');
-    entity1.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+    entity1setAttribute('scale', '20 20 20');
+    entity1.setAttribute('gps-entity-place', `latitude: ${latitude1}; longitude: ${longitude1}`);
     scene.appendChild(entity1);
 
     const textEntity1 = document.createElement('a-entity');
-    textEntity1.setAttribute('text', `value: ${text}; color: black; align: center; width: 10;`);
+    textEntity1.setAttribute('text', `value: ${text1}; color: black; align: center; width: 10;`);
     textEntity1.setAttribute('position', '0 5 0');
     entity.appendChild(textEntity1);
 }
-createARObject(latitude, longitude, text);
-createARObject(latitude1, longitude1, text1);
+createARObject(latitude, longitude, text, latitude1, longitude1, text1);
