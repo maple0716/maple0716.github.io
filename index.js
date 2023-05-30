2194813e5294ad8e60418152e06f42eb9fa23121
maple0716 <121430876+maple0716@users.noreply.github.com>
2023-05-30 16:11:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,139 @@
-AFRAME.registerComponent('placetext', {
-    schema: {
-        latitude: { type: 'number' },
-        longitude: { type: 'number' },
-        text: { type: 'string' },
-        scale: { type: 'number', default: 10 }
-    },
-    init: function () {
-        const textScale = this.data.scale * 100;
+function main() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(80, 2, 0.1, 50000);
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#canvas1')
+    });
 
-        // Create text entity
-        const textEntity = document.createElement('a-text');
-        textEntity.setAttribute('value', this.data.text);
-        textEntity.setAttribute('material', 'color: black;');
-        textEntity.setAttribute('scale', {
-            x: textScale,
-            y: textScale,
-            z: textScale
-        });
-        textEntity.setAttribute('align', 'center');
-        textEntity.setAttribute('position', '0 0.5 0');
+    const geom = new THREE.BoxGeometry(20, 20, 20);
+    const arjs = new THREEx.LocationBased(scene, camera);
 
-        // Create place entity
-        const placeEntity = document.createElement('a-entity');
-        placeEntity.setAttribute('gps-projected-entity-place', {
-            latitude: this.data.latitude,
-            longitude: this.data.longitude,
-        });
-        placeEntity.setAttribute('look-at', '[gps-projected-camera]');
+    // You can change the minimum GPS accuracy needed to register a position - by default 1000m
+    //const arjs = new THREEx.LocationBased(scene, camera. { gpsMinAccuracy: 30 } );
+    const cam = new THREEx.WebcamRenderer(renderer, '#video1');
 
-        // Add text entity to place entity
-        placeEntity.appendChild(textEntity);
+    const mouseStep = THREE.MathUtils.degToRad(5);
 
-        // Append place entity to scene
-        const scene = document.querySelector('#ENTITY');
-        scene.appendChild(placeEntity);
+
+    let orientationControls;
+
+    // Orientation controls only work on mobile device
+    if (isMobile()) {
+        orientationControls = new THREEx.DeviceOrientationControls(camera);
     }
-});
+    
+    let fake = null;
+        const locations = [
+            { latitude: 31.0271378, longitude: 121.4470954, text: 'Hello, Location 1!' },
+            { latitude: 31.0269501, longitude: 121.4472652, text: 'Hello, Location 2!' },
+            { latitude: 31.0268305, longitude: 121.4473130, text: 'Hello, Location 3!' },
+        ]
+    Locations.forEach((location) => {
+        setupObjects(location.longitude, location.latitude);
+    });
 
-document.addEventListener('DOMContentLoaded', function () { 
-    const locations = [
-        { latitude: 31.0271378, longitude: 121.4470954, text: 'Hello, Location 1!' },
-        { latitude: 31.0269501, longitude: 121.4472652, text: 'Hello, Location 2!' },
-        { latitude: 31.0268305, longitude: 121.4473130, text: 'Hello, Location 3!' },
-    ];
-    const entityContainer = document.querySelector('a-scene');
-    for (let i = 0; i < locations.length; i++) {
-        const entity = document.createElement('a-entity');
-        entity.setAttribute('placetext', {
-            latitude: locations[i].latitude,
-            longitude: locations[i].longitude,
-            text: locations[i].text
-            
-        });
-        entityContainer.appendChild(entity);
-        console.log(locations[i].latitude);
+   
+ 
+    function isMobile() {
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            // true for mobile device
+            return true;
+        }
+        return false;
     }
-}); 
 
+    function render(time) {
+        resizeUpdate();
+        if (orientationControls) orientationControls.update();
+        cam.update();
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+
+    function resizeUpdate() {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth, height = canvas.clientHeight;
+        if (width != canvas.width || height != canvas.height) {
+            renderer.setSize(width, height, false);
+        }
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
+
+    function setupObjects(targetlongitude, targetlatitude) {
+        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        const material2 = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        const material3 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+        const material4 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube1 = new THREE.Mesh(geom, material);
+        const cube2 = new THREE.Mesh(geom, material2);
+        const cube3 = new THREE.Mesh(geom, material3);
+        const cube4 = new THREE.Mesh(geom, material4);
+
+        // 根据手机距离立方体的距离，控制立方体的可见性
+        arjs.on("gpsupdate", pos => {
+            const longitudeDiff = Math.abs(pos.coords.longitude - targetLongitude);
+            const latitudeDiff = Math.abs(pos.coords.latitude - targetLatitude);
+            if (longitudeDiff < 0.0001 && latitudeDiff < 0.0001) {
+                cube1.visible = true;
+                cube2.visible = true;
+                cube3.visible = true;
+                cube4.visible = true;
+            } else {
+                cube1.visible = false;
+                cube2.visible = false;
+                cube3.visible = false;
+                cube4.visible = false;
+            }
+            arjs.add(cube1, pos.coords.longitude, pos.coords.latitude + 0.001);
+            /* arjs.add(cube2, pos.coords.longitude, pos.coords.latitude - 0.001); // slightly south
+            arjs.add(cube3, pos.coords.longitude - 0.001, pos.coords.latitude); // slightly west
+            arjs.add(cube4, pos.coords.longitude + 0.001, pos.coords.latitude); */
+
+        });
+        arjs.on("gpserror", code => {
+            alert(`GPS error: code ${code}`);
+        });
+        // Uncomment to use a fake GPS location
+        //fake = { lat: 51.05, lon : -0.72 };
+        if (fake) {
+            arjs.fakeGps(fake.lon, fake.lat);
+        } else {
+            arjs.startGps();
+        }
+        let mousedown = false, lastX = 0;
+
+        // Mouse events for testing on desktop machine
+        if (!isMobile()) {
+            window.addEventListener("mousedown", e => {
+                mousedown = true;
+            });
+
+            window.addEventListener("mouseup", e => {
+                mousedown = false;
+            });
+
+            window.addEventListener("mousemove", e => {
+                if (!mousedown) return;
+                if (e.clientX < lastX) {
+                    camera.rotation.y += mouseStep;
+                    if (camera.rotation.y < 0) {
+                        camera.rotation.y += 2 * Math.PI;
+                    }
+                } else if (e.clientX > lastX) {
+                    camera.rotation.y -= mouseStep;
+                    if (camera.rotation.y > 2 * Math.PI) {
+                        camera.rotation.y -= 2 * Math.PI;
+                    }
+                }
+                lastX = e.clientX;
+            });
+        }
+        
+    }
+      requestAnimationFrame(render);
+
+    
+}
+
+main();
